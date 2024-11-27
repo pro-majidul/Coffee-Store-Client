9371a70db683bfacf974fdfe8f6dc16da11491c4
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-11-28 04:10:30 +0600

    coffee updated successfully

--- src/main.jsx
+++ src/main.jsx
@@ -28,7 +28,8 @@ const router = createBrowserRouter([
         element: <AddCoffee></AddCoffee>
       }, {
         path: "updateCoffee/:id",
-        element: <UpdateCoffee></UpdateCoffee>
+        element: <UpdateCoffee></UpdateCoffee>,
+        loader : ({params})=>fetch(`http://localhost:3000/coffee/${params.id}`)
       }
     ]
   },
