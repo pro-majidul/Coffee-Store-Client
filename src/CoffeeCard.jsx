aac7572b578fe0bf94e00a10040b18c8eee41411
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-11-27 15:07:04 +0600

    coffee card section added

--- src/CoffeeCard.jsx
+++ src/CoffeeCard.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { IoEye } from 'react-icons/io5';
+import { MdDelete, MdEdit } from 'react-icons/md';
 
-const CoffeeCard = ({coffee}) => {
+const CoffeeCard = ({ coffee }) => {
     console.log(coffee);
     return (
-        <div>
-            
+        <div className='bg-[#F5F4F1] p-3 -z-50 opacity-100 flex items-center justify-between'>
+            <div>
+                <img src={coffee.photo} alt={coffee.name} />
+            </div>
+            <div>
+                <p>Name : {coffee.name}</p>
+                <p>Chef : {coffee.suplier}</p>
+                <p>Price : {coffee.chef}</p>
+            </div>
+            <div className='space-y-1'>
+                <p className='bg-blue-500  p-1 rounded-lg'><IoEye size={24} color='white' /> </p>
+                <p  className="bg-black  p-1 rounded-lg"><MdEdit size={24} color='white' /></p>
+               <p  className="bg-red-500  p-1 rounded-lg"> <MdDelete size={24} color='white' /></p>
+            </div>
         </div>
     );
 };
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,9 +3,14 @@ import Banner from './components/Banner';
 import Feature from './components/Feature';
 import { useLoaderData } from 'react-router-dom';
 import CoffeeCard from './CoffeeCard';
+import { BsCup } from 'react-icons/bs';
+import myImage from './assets/images/more/4.png'
+import myImage2 from './assets/images/more/5.png'
+
 
 const Home = () => {
     const Coffees = useLoaderData();
+
     return (
         <div>
 
@@ -13,10 +18,28 @@ const Home = () => {
             <div className='bg-[#ECEAE3]'>
                 <Feature></Feature>
             </div>
+            <section className='py-10'>
+            <div className='space-y-3' style={{
+                backgroundImage: `url(${myImage}), url(${myImage2})`,
+                backgroundPosition: 'left top, right bottom',
+                backgroundRepeat: 'no-repeat, no-repeat',
+                zIndex: 10,
+                
+            }} >
+                <p className='text-center'>--Sip & Savor---</p>
+                <h3 className='text-center md:text-3xl text-xl text-[#331A15]'>Our Popular Products</h3>
+                <div className='flex items-center justify-center'>
+                    <button className='px-4 flex items-center gap-1 text-xl py-2 bg-[#E3B577] border-2 border-black hover:bg-[#331A15] rounded-lg  text-white'>Add Coffee <BsCup size={24} color='#331A15' /></button>
+                </div>
+               <div className='grid md:grid-cols-2 py-5 -z-50 w-11/12 mx-auto gap-5'>
+               {
+                    Coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+                }
+               </div>
+            </div>
+            </section>
 
-            {
-                Coffees.map(coffee =><CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
-            }
+
 
         </div>
     );
